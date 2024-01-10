9980d2f072c3609947e2131e7b8eef2fc29c1511
cdenq <christopherdenq@gmail.com>
2024-01-10 02:47:52 -0500

    streamlit changed their deployment filepathing; fixed to update to it

--- modules/eda.py
+++ modules/eda.py
@@ -1,8 +1,8 @@
 #----------------------------------------------------
 # Imports
 #----------------------------------------------------
-import imports
-import helper
+from modules import imports
+from modules import helper
 
 #----------------------------------------------------
 # Helper Functions for EDA
